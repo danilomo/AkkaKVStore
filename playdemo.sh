629708a9a284345dbef38f23e79b135dfd1a636b
Danilo Oliveira <danilomendoncaoliveira@gmail.com>
2019-02-21 13:16:47 +0100

    Hardcoding IP address in the receiver main class for testing purposes, this will be fixed later

--- playdemo.sh
+++ playdemo.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+cd deployment
+
+echo "Starting frontend nodes"
+ansible-playbook start_nodes.yml -e "role=frontend"
+sleep 5
+
+echo "Starting backend nodes"
+ansible-playbook start_nodes.yml -e "role=backend"
+sleep 5
+
+echo "Starting senders and receivers"
+ansible-playbook start_senders_and_receivers.yml
