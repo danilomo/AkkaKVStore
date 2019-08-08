80e7912a56a2ea0c4360fe71547b5920cf497a7c
Danilo Oliveira <danilomendoncaoliveira@gmail.com>
2019-08-08 16:15:03 +0200

    Extends scripts to perform a network partition of a vagrant cluster via IP tables rules

--- deployment/cluster_config.rb
+++ deployment/cluster_config.rb
@@ -1,40 +1,32 @@
 require 'set'
 
-class Cluster  
+NO_CONFIG = Proc.new { |b| nil }
 
-  @@NO_CONFIG = Proc.new { |b| nil }
-  
+class Cluster  
   def initialize(
         base_ip: "192.168.1",
         senders: 0,
         receivers: 0,
         frontends: 2,
-        backends: 2
+        backends: 2,
+        specific_config: {}
       )
     
     @base_ip = base_ip
     @senders   = senders
     @receivers = receivers
     @frontends = frontends
-    @backends  = backends
-    
+    @backends  = backends    
     @seed_nodes = ((1..@frontends).map { |n| "\"akka.tcp://kvstore@#{@base_ip}.#{n + 1}:2551\"" }).join(", ")
     @groups = { "backend" => [], "frontend" => [], "sender" => [], "receiver" => [] }
-
-    @specific_config = {
-    }
+    @specific_config = specific_config
   end
 
-  def seed_nodes()
-    @seed_nodes
-  end
-
-  def groups()
-    @groups
-  end
+  attr_reader :seed_nodes
+  attr_reader :groups
   
   def apply_specific_config(name, box)
-    block = @specific_config.fetch(name, @@NO_CONFIG)
+    block = @specific_config.fetch(name, NO_CONFIG)
     block.call(box)
   end
 
@@ -44,7 +36,7 @@ class Cluster
     vb.cpus = 1
   end
 
-  def list_of_nodes()
+  def list_of_nodes
     indexes      = (1..(@frontends + @backends + @senders + @receivers)).to_a  
     sufixes      = [ (1 .. @frontends).to_a, (1 .. @backends).to_a, (1 .. @senders).to_a, (1 .. @receivers).to_a ].flatten
     roles        = [ ["frontend"] * @frontends, ["backend"] * @backends, ["sender"] * @senders, ["receiver"] * @receivers ].flatten
@@ -56,18 +48,29 @@ class Cluster
   end
 
   def split_network(nodes)
+    alter_network(nodes,"disconnect_from")
+  end
+
+  def merge_network(nodes)
+    alter_network(nodes,"connect_to")
+  end  
+  
+  def alter_network(nodes, action)
     node_set = nodes.to_set
     other_partition = list_of_nodes()
                         .select { |node| ! node_set.member?(node[0]) }
                         .map { |node| node[1] }
+                        .join(" ")
 
     nodes.each { |src|
-      other_partition.each { |dst|
-        puts src + ", " + dst
-      }
+      `vagrant ssh #{src} -c 'sudo #{action} #{other_partition}'`
     }
-    
   end
 
+  def crash_node(node)
+  end
+
+  def freeze_node(node)
+  end
 
 end
